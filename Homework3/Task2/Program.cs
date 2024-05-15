5702e58f1e7dd9916085ee101a18d6f928fc6362
kapche <arthurkapche@gmail.com>
2024-05-15 18:53:55 +0400

    Homework 3

--- Homework3/Task2/Program.cs
+++ Homework3/Task2/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿struct Time
+{
+    public int Hour;
+    public int Minute;
+    public int Second;
+}
 
+class Program
+{
+    static void Main()
+    {
+        Time currentTime;
+        currentTime.Hour = 12;
+        currentTime.Minute = 30;
+        currentTime.Second = 45;
+
+        bool isTimeValid = CheckTime(currentTime);
+        Console.WriteLine(isTimeValid);
+    }
+
+    static bool CheckTime(Time time)
+    {
+        if (time.Hour >= 0 && time.Hour < 24 && time.Minute >= 0 && time.Minute < 60 && time.Second >= 0 && time.Second < 60)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- Homework3/Task4/Task4/Program.cs
+++ Homework3/Task4/Task4/Program.cs
@@ -1,17 +1,33 @@
-﻿struct Point
+﻿class Song
 {
-    public int X;
-    public int Y;
+    public string Artist { get; set; }
+    public string Title { get; set; }
+    public int DurationInSeconds { get; set; }
+
+    public Song(string artist, string title, int durationInSeconds)
+    {
+        Artist = artist;
+        Title = title;
+        DurationInSeconds = durationInSeconds;
+    }
+
+    public string GetFormattedDuration()
+    {
+        int minutes = DurationInSeconds / 60;
+        int seconds = DurationInSeconds % 60;
+        return $"{minutes}:{seconds:D2}";
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        Point point1 = new Point { X = 1, Y = 2 };
-        Point point2 = new Point { X = 4, Y = 6 };
+        Song song1 = new Song("Imagine Dragons", "Believer", 204); 
+        Song song2 = new Song("Ed Sheeran", "Shape of You", 232); 
 
-        double distance = Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point1.Y, 2));
-        Console.WriteLine($"Длина отрезка между точками: {distance}");
+        Song shorterSong = song1.DurationInSeconds < song2.DurationInSeconds ? song1 : song2;
+
+        Console.WriteLine($"{shorterSong.Artist} - {shorterSong.Title} {shorterSong.GetFormattedDuration()}");
     }
 }
