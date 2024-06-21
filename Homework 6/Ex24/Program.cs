9f8b4447a486ac4befa1d4dbdb067172062a9201
kapche <arthurkapche@gmail.com>
2024-06-21 17:24:13 +0400

    Homework -6

---
+++ Homework 6/Ex24/Program.cs
@@ -0,0 +1,17 @@
+﻿class ArrayService
+{
+    public int[] GetSubArray(int[] arr, int startIndex, int endIndex)
+    {
+        if (startIndex < 0 || endIndex >= arr.Length || startIndex > endIndex)
+        {
+            throw new ArgumentException("Invalid start or end index for subarray");
+        }
+
+        int subArrayLength = endIndex - startIndex + 1;
+        int[] subArray = new int[subArrayLength];
+
+        Array.Copy(arr, startIndex, subArray, 0, subArrayLength);
+
+        return subArray;
+    }
+}
