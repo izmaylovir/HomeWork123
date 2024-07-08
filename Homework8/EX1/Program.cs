503df4aba6b0be5a27a6dcc53b3ec2f55a9e876f
kapche <arthurkapche@gmail.com>
2024-07-08 18:01:04 +0400

    Homework 8

---
+++ Homework8/EX1/Program.cs
@@ -0,0 +1,18 @@
+﻿class DateGenerator
+{
+    private static readonly Random random = new Random();
+
+    public static DateTime GenerateRandomDate(int year)
+    {
+        int day = random.Next(1, 29); 
+        int month = random.Next(1, 13); 
+        return new DateTime(year, month, day);
+    }
+
+    static void Main()
+    {
+        int year = 2022; 
+        DateTime randomDate = GenerateRandomDate(year);
+        Console.WriteLine("Сгенерированная дата: " + randomDate.ToString("dd/MM/yyyy"));
+    }
+}
---
+++ Homework8/EX6/Program.cs
@@ -0,0 +1,28 @@
+﻿class DateHelper
+{
+    public static DateTime[] GetDateRange(DateTime startDate, DateTime endDate)
+    {
+        int daysCount = (int)(endDate - startDate).TotalDays + 1;
+        DateTime[] dateRange = new DateTime[daysCount];
+
+        for (int i = 0; i < daysCount; i++)
+        {
+            dateRange[i] = startDate.AddDays(i);
+        }
+
+        return dateRange;
+    }
+
+    static void Main()
+    {
+        DateTime startDate = new DateTime(2022, 10, 1); // Пример начальной даты
+        DateTime endDate = new DateTime(2022, 10, 10); // Пример конечной даты
+        DateTime[] range = GetDateRange(startDate, endDate);
+
+        Console.WriteLine("Даты в заданном промежутке:");
+        foreach (DateTime date in range)
+        {
+            Console.WriteLine(date.ToShortDateString());
+        }
+    }
+}
