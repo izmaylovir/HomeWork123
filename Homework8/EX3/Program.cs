30978702b6784203c498552dc5c3d1a9663d50d8
kapche <arthurkapche@gmail.com>
2024-07-10 18:01:31 +0400

    Homework_8

--- Homework8/EX3/Program.cs
+++ Homework8/EX3/Program.cs
@@ -3,13 +3,13 @@
     public static DateTime GetEndOfWeek(DateTime date)
     {
         DayOfWeek dayOfWeek = date.DayOfWeek;
-        int daysToAdd = 7 - (int)dayOfWeek; 
+        int daysToAdd = 6 - (int)dayOfWeek; 
         return date.AddDays(daysToAdd);
     }
 
     static void Main()
     {
-        DateTime inputDate = new DateTime(2022, 10, 15); 
+        DateTime inputDate = new DateTime(2024, 9, 10); 
         DateTime endOfWeek = GetEndOfWeek(inputDate);
         Console.WriteLine("Дата окончания недели для {0} : {1}", inputDate.ToString("dd/MM/yyyy"), endOfWeek.ToString("dd/MM/yyyy"));
     }
