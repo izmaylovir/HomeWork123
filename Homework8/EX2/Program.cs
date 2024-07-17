5fe4b60fa527499a04bdcecbc44c130d1bb4ecea
izmaylovir <167544577+izmaylovir@users.noreply.github.com>
2024-07-17 22:16:01 +0300

    Homework 8 (#8)
    * Homework 8
    
    * Homework_8
    
    * Homework8rework

---
+++ Homework8/EX2/Program.cs
@@ -0,0 +1,16 @@
+﻿class DateHelper
+{
+    public static DateTime GetStartOfWeek(DateTime date)
+    {
+        DayOfWeek dayOfWeek = date.DayOfWeek;
+        int daysToSubtract = (int)dayOfWeek - 1; 
+        if (daysToSubtract < 0) daysToSubtract += 7; 
+        return date.AddDays(-daysToSubtract);
+    }
+    static void Main()
+    {
+        DateTime inputDate = new DateTime(2024, 10, 15); 
+        DateTime startOfWeek = GetStartOfWeek(inputDate);
+        Console.WriteLine("Дата начала недели для {0} : {1}", inputDate.ToString("dd/MM/yyyy"), startOfWeek.ToString("dd/MM/yyyy"));
+    }
+}
---
+++ Homework8/EX5/Program.cs
@@ -0,0 +1,26 @@
+﻿class DateHelper
+{
+    public static int CountWeekendDaysInRange(DateTime startDate, DateTime endDate)
+    {
+        int count = 0;
+
+        for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+        {
+            if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static void Main()
+    {
+        DateTime startDate = new DateTime(2022, 10, 1); 
+        DateTime endDate = new DateTime(2022, 10, 31); 
+        int weekendDaysCount = CountWeekendDaysInRange(startDate, endDate);
+
+        Console.WriteLine($"Количество выходных дней в заданном промежутке: {weekendDaysCount}");
+    }
+}
